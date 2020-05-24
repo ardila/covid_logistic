2069c5c2bf29b96d8032116e61724127095e0917
Diego Ardila <ardila.d.ardila@gmail.com>
2020-05-24 16:37:02 -0700

    auto update

--- update_plots.py
+++ update_plots.py
@@ -13,59 +13,81 @@ import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
 import datetime
+
 url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 s = requests.get(url).content
-c = pd.read_csv(io.StringIO(s.decode('utf-8')))
+c = pd.read_csv(io.StringIO(s.decode("utf-8")))
 url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 s = requests.get(url).content
-d = pd.read_csv(io.StringIO(s.decode('utf-8')))
+d = pd.read_csv(io.StringIO(s.decode("utf-8")))
 model = LinearRegression()
 today = datetime.datetime.today().strftime("%B %d, %Y")
-plt.rcParams.update({'font.size': 30})
-for data_type in ('cases', 'deaths'):
+plt.rcParams.update({"font.size": 30})
+for data_type in ("cases", "deaths"):
     legends = []
     plt.figure(figsize=(20, 15))
-    for country, color in (('Italy', 'r'), ('US', 'b'), ('China', 'g'), ('Iran', 'y'), ('Spain', 'purple'), ('India', 'black'), ('Brazil', 'pink')):
+    for country, color in (
+        ("Italy", "r"),
+        ("US", "b"),
+        ("China", "g"),
+        ("Iran", "y"),
+        ("Spain", "purple"),
+        ("India", "black"),
+        ("Brazil", "pink"),
+    ):
+
         def filter(x):
-            return country == x['Country/Region']
+            return country == x["Country/Region"]
+
         selected_data = d[d.apply(filter, axis=1)]
         selected_data_deaths = selected_data.iloc[:, 4:].sum(axis=0)
         selected_data = c[c.apply(filter, axis=1)]
         selected_data_confirmed_cases = selected_data.iloc[:, 4:].sum(axis=0)
-        if data_type == 'deaths':
+        if data_type == "deaths":
             last_zero_day = np.argwhere(selected_data_deaths.values < 50).max()
             start = last_zero_day + 1
             xdata = selected_data_deaths.values[start:]
-            ydata = selected_data_deaths.diff(
-            ).values[start:]/selected_data_deaths.values[start-1:-1]
-        elif data_type == 'cases':
+            ydata = (
+                selected_data_deaths.diff().values[start:]
+                / selected_data_deaths.values[start - 1 : -1]
+            )
+        elif data_type == "cases":
             last_zero_day = np.argwhere(selected_data_deaths.values < 50).max()
             start = last_zero_day + 1
             xdata = selected_data_confirmed_cases.values[start:]
-            ydata = selected_data_confirmed_cases.diff(
-            ).values[start:]/selected_data_confirmed_cases.values[start-1:-1]
+            ydata = (
+                selected_data_confirmed_cases.diff().values[start:]
+                / selected_data_confirmed_cases.values[start - 1 : -1]
+            )
         plt.scatter(xdata, ydata, color=color)
 
-        model.fit(xdata.reshape((-1, 1))[-7:], ydata[-7:], sample_weight=xdata[-7:])
-        x_intercept = -model.intercept_/model.coef_
+        model.fit(
+            xdata.reshape((-1, 1))[-14:], ydata[-14:], sample_weight=xdata[-14:]
+        )
+        x_intercept = -model.intercept_ / model.coef_
         try:
             x_intercept = int(np.round(x_intercept[0]))
-            if x_intercept<0:
-                x_intercept = 'TOO NOISY'
+            if x_intercept < 0:
+                x_intercept = "TOO NOISY"
         except:
-            x_intercept = 'TOO NOISY'
+            x_intercept = "TOO NOISY"
         info = "Predicted total %s in %s: %s" % (
-            data_type, country, x_intercept)
+            data_type,
+            country,
+            x_intercept,
+        )
         print(info)
         legends.append(info)
         # if x_intercept > 0:
         new_x = np.arange(max(xdata)).reshape(-1, 1)
         plt.plot(new_x, model.predict(new_x), color=color)
-        plt.xlabel('Number of %s' % data_type)
-        plt.ylabel('Percent growth (diff/ number confirmed)')
+        plt.xlabel("Number of %s" % data_type)
+        plt.ylabel("Percent growth (diff/ number confirmed)")
         plt.grid()
-        plt.ylim([0, .5])
+        plt.ylim([0, 0.5])
     plt.legend(legends)
-    plt.title('Logistic Model Predictions of %s for %s' %
-              (data_type, today), fontsize=40)
-    plt.savefig('%s.png' % data_type)
+    plt.title(
+        "Logistic Model Predictions of %s for %s" % (data_type, today),
+        fontsize=40,
+    )
+    plt.savefig("%s.png" % data_type)
